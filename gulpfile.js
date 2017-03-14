09468b0482b48d25c60e5e37e742882b1980ccf8
BuildTools <michaeljgerace@gmail.com>
2017-03-14 17:09:08 -0400

    Fix safari pls

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 // Sass configuration
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var src = "static/css/*.scss"
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer');
 
-gulp.task('sass', function() {
-    gulp.src(src)
+var paths = {
+    scss: './static/css/*.scss'
+};
+
+gulp.task('styles', function() {
+    gulp.src(paths.scss)
         .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
 });
 
-gulp.task('default', ['sass'], function() {
-    gulp.watch(src, ['sass']);
-})
+gulp.task('default', function() {
+    gulp.watch(paths.scss, ['styles']);
+});
